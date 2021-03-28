6233270efc9a20254bf7171d2030984e8919736f
Zhao Shu <zhaobenben007@googlemail.com>
2021-03-28 17:18:51 +0200

    update

--- projects/DensePose/infer_segm.py
+++ projects/DensePose/infer_segm.py
@@ -14,70 +14,101 @@ from densepose.vis.densepose_results import DensePoseResultsFineSegmentationVisu
 from densepose.vis.bounding_box import ScoredBoundingBoxVisualizer
 from densepose.vis.extractor import CompoundExtractor, DensePoseResultExtractor, create_extractor
 import argparse
+from pathlib import Path
 
-parser = argparse.ArgumentParser(description='DensePose - Infer segments')
-parser.add_argument('--input', help='Path to image')
-args = parser.parse_args()
 
-infile = args.input
-fname = infile[infile.find('/')+1:infile.rfind('.')]
-outfile = os.path.join('pix', fname + '_segm.jpg')
+def generate_segm(infile, score_cutoff, show):
 
-im = cv2.imread(infile)
-# cv2.imshow('input', im)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+    print('input:', infile)
 
-cfg = get_cfg()
-add_densepose_config(cfg)
-cfg.MODEL.DEVICE = 'cpu'
+    image = cv2.imread(infile)
+    # cv2.imshow('input', image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
-cfg.merge_from_file("./configs/densepose_rcnn_R_50_FPN_s1x.yaml")
-cfg.MODEL.WEIGHTS = "./models/densepose_rcnn_R_50_FPN_s1x.pkl"
+    cfg = get_cfg()
+    add_densepose_config(cfg)
+    cfg.MODEL.DEVICE = 'cpu'
 
-predictor = DefaultPredictor(cfg)
-outputs = predictor(im)
+    cfg.merge_from_file('./configs/densepose_rcnn_R_50_FPN_s1x.yaml')
+    cfg.MODEL.WEIGHTS = './models/densepose_rcnn_R_50_FPN_s1x.pkl'
 
-# print(outputs["instances"].pred_classes)
-# print(outputs["instances"].pred_boxes)
-print(outputs["instances"].pred_densepose)
+    predictor = DefaultPredictor(cfg)
+    outputs = predictor(image)
 
-# filter the probabilities of scores for each bbox > 90%
-instances = outputs["instances"]
-confident_detections = instances[instances.scores > 0.9]
+    # print(outputs["instances"].pred_classes)
+    # print(outputs["instances"].pred_boxes)
+    # print(outputs["instances"].pred_densepose)
 
-visualizers = []
+    # filter the probabilities of scores for each bbox > 90%
+    instances = outputs['instances']
+    confident_detections = instances[instances.scores > score_cutoff]
 
-visualizer_segm = DensePoseResultsFineSegmentationVisualizer(cfg=cfg)
-visualizer_bbox = ScoredBoundingBoxVisualizer(cfg=cfg)
+    visualizers = []
 
-visualizers.append(visualizer_segm)
-visualizers.append(visualizer_bbox)
+    visualizer_segm = DensePoseResultsFineSegmentationVisualizer(cfg=cfg)
+    visualizer_bbox = ScoredBoundingBoxVisualizer(cfg=cfg)
 
-extractors = []
+    visualizers.append(visualizer_segm)
+    visualizers.append(visualizer_bbox)
 
-extractor_segm = create_extractor(visualizer_segm)
-extractor_bbox = create_extractor(visualizer_bbox)
+    extractors = []
 
-extractors.append(extractor_segm)
-extractors.append(extractor_bbox)
+    extractor_segm = create_extractor(visualizer_segm)
+    extractor_bbox = create_extractor(visualizer_bbox)
 
-visualizer = CompoundVisualizer(visualizers)
-extractor = CompoundExtractor(extractors)
+    extractors.append(extractor_segm)
+    extractors.append(extractor_bbox)
 
-im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-im_gray = np.tile(im_gray[:, :, np.newaxis], [1, 1, 3])
+    visualizer = CompoundVisualizer(visualizers)
+    extractor = CompoundExtractor(extractors)
 
-# instances
-# data = extractor(instances)
+    im_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    im_gray = np.tile(im_gray[:, :, np.newaxis], [1, 1, 3])
 
-# confident detections
-data = extractor(confident_detections)
+    # instances
+    # data = extractor(instances)
 
-im_vis = visualizer.visualize(im_gray, data)
+    # confident detections
+    data = extractor(confident_detections)
 
-cv2.imshow('segm', im_vis)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    image_vis = visualizer.visualize(im_gray, data)
 
-cv2.imwrite(outfile, im_vis)
+    if show:
+        cv2.imshow('segm', image_vis)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    else:
+        outfile = generate_outfile(infile)
+        cv2.imwrite(outfile, image_vis)
+        print('output:', outfile)
+
+
+def generate_outfile(infile):
+
+    outdir = os.path.join('output', infile[infile.find('/') + 1:infile.rfind('/')])
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    fname = infile[infile.find('/') + 1:infile.rfind('.')]
+    outfile = os.path.join('output', '{}_segm.jpg'.format(fname))
+
+    return outfile
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='DensePose - Infer the segments')
+    parser.add_argument('--input', help='Path to image file or directory')
+    args = parser.parse_args()
+
+    if os.path.isfile(args.input):
+        generate_segm(infile=args.input, score_cutoff=0.9, show=True)
+
+    elif os.path.isdir(args.input):
+        for path in Path(args.input).rglob('*.jpg'):
+            generate_segm(infile=str(path), score_cutoff=0.9, show=False)
+    else:
+        pass
